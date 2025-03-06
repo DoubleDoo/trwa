9a2098958355aba1908eceded2f2f894853f9d8d
Dubinich <dodubina@gmail.com>
2025-03-07 02:43:56 +0300

    qrfqwefwef

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -7,12 +7,11 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 export default function FutureGamePage() {
     const [loading, setLoading] = useState(true);
 
-    // Load Unity assets and manage loading state
-    const { unityProvider, isLoading, isLoaded } = useUnityContext({
-        loaderUrl: "./build/tg_survivor.loader.js",
-        dataUrl: "./build/tg_survivor.data.gz",
-        frameworkUrl: "./build/tg_survivor.framework.js.gz",
-        codeUrl: "./build/tg_survivor.wasm.gz",
+    const { addEventListener, removeEventListener, unityProvider, isLoading, isLoaded } = useUnityContext({
+        loaderUrl: "/Build/WebBuild.loader.js",
+        dataUrl: "/Build/WebBuild.data.gz",
+        frameworkUrl: "/Build/WebBuild.framework.js",
+        codeUrl: "/Build/WebBuild.wasm.gz",
     });
 
     // Use useEffect to handle the Unity loading state
@@ -25,6 +24,5 @@ export default function FutureGamePage() {
     if (loading || isLoading) {
         return <div>Loading...</div>; // Display loading indicator until Unity is ready
     }
-
     return <Unity unityProvider={unityProvider} />;
 }
