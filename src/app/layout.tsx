0498a1b45f282dce76793d6a12218fd92369bab1
Dubinich <dodubina@gmail.com>
2025-03-06 19:09:13 +0300

    wedwedwedq

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout(props: { children: ReactNode }) {
   return (
       <html lang="en">
       <head>
-          <script src="https://telegram.org/js/telegram-web-app.js"></script>
+          <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
       </head>
       <body className={inter.className}>
       <Providers initialState={initialState}>{props.children}</Providers>
--- src/tgapi.ts
+++ src/tgapi.ts
@@ -5,7 +5,7 @@ const useTelegram = () => {
 
     useEffect(() => {
         const script = document.createElement("script");
-        script.src = "https://telegram.org/js/telegram-web-app.js";
+        script.src = "https://telegram.org/js/telegram-web-app.js?56";
         script.async = true;
         script.onload = () => {
             if (window.Telegram?.WebApp) {
