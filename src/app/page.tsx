3c7692f44daea0695281f79e2186cd031367a5fe
Dubinich <dodubina@gmail.com>
2025-03-06 18:07:19 +0300

    wedwedw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,36 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
+        <h2>TEST</h2>
       </div>
     </>
   )
