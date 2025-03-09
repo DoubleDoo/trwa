14505d2d82436323ceb88b3e2c50ffb348f43e68
Dubinich <dodubina@gmail.com>
2025-03-10 02:39:16 +0300

    qewfqwef

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -5,7 +5,7 @@ import {Fragment, useEffect, useState} from "react";
 
 export default function FutureGamePage() {
     const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-        loaderUrl: "/tg_survivor/tg_survivor.loader.js",
+        loaderUrl: "/tg_survivor_js/tg_survivor.loader.js",
         dataUrl: "/tg_survivor/tg_survivor.data.gz",
         frameworkUrl: "/tg_survivor/tg_survivor.framework.js.gz",
         codeUrl: "/tg_survivor/tg_survivor.wasm.gz",
@@ -18,7 +18,6 @@ export default function FutureGamePage() {
             )}
             <Unity
                 unityProvider={unityProvider}
-                style={{ visibility: isLoaded ? "visible" : "hidden" }}
             />
         </Fragment>
     );
