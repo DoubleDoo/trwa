7c0e08d2ea9a02980d0de8dc283a90df378e055f
Dubinich <dodubina@gmail.com>
2025-03-09 19:27:25 +0300

    asdcasdcasdc

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -8,10 +8,10 @@ export default function FutureGamePage() {
     const [loading, setLoading] = useState(true);
 
     const { addEventListener, removeEventListener, unityProvider, isLoading, isLoaded } = useUnityContext({
-        loaderUrl: "/build/WebBuild.loader.js",
-        dataUrl: "/build/WebBuild.data.gz",
-        frameworkUrl: "/build/WebBuild.framework.js",
-        codeUrl: "/build/WebBuild.wasm.gz",
+        loaderUrl: "/tg_survivor.loader.js",
+        dataUrl: "/tg_survivor.data.gz",
+        frameworkUrl: "/tg_survivor.framework.js.gz",
+        codeUrl: "/tg_survivor.wasm.gz",
     });
     return <Unity unityProvider={unityProvider} />;
 }
